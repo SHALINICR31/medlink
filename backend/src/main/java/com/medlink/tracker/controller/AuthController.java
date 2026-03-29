b2a0648ba23f42c264e835c0691008eea67dd843
CR SHALINI <mshaisha16@gmail.com>
2026-03-30 00:16:39 +0530

    Update AuthController.java

--- backend/src/main/java/com/medlink/tracker/controller/AuthController.java
+++ backend/src/main/java/com/medlink/tracker/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
